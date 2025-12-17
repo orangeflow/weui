23c92c34d6f20170f844372aaa24fa2d50411706
flowwang <ginggogo@qq.com>
2025-12-17 17:48:52 +0800

    Refactor payment interface by optimizing event handling in app.js and updating styles for better responsiveness. Remove deprecated components and clean up related CSS. Adjust index.html for improved layout consistency.

--- components/RedPacket/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as RedPacketCard } from './RedPacketCard'
-export { default as RedPacketIcon } from './RedPacketIcon'
-export { default as RedPacketButton } from './RedPacketButton'
-export { default as RedPacketList } from './RedPacketList'
-
